5c9fb17c151d01904807888adb3b54b66cb2a4ec
Apophuy <apophuy@hotmail.com>
2020-08-13 22:22:12 +0500

    Updates

---
+++ precache-manifest.2824549cf2dbadc3696395a314d7b649.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6de97430c5fe45b0a2f872ea51af24f6",
+    "url": "/realworld/index.html"
+  },
+  {
+    "revision": "761124469c14e92c6ea5",
+    "url": "/realworld/static/css/2.56808b57.chunk.css"
+  },
+  {
+    "revision": "2077dc20f2f5b78fb174",
+    "url": "/realworld/static/css/main.d095d89e.chunk.css"
+  },
+  {
+    "revision": "761124469c14e92c6ea5",
+    "url": "/realworld/static/js/2.50940dc6.chunk.js"
+  },
+  {
+    "revision": "4f98be436b88b05bb27c5253e1cce92a",
+    "url": "/realworld/static/js/2.50940dc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2077dc20f2f5b78fb174",
+    "url": "/realworld/static/js/main.4b7c2f7b.chunk.js"
+  },
+  {
+    "revision": "8b918f932a61fe9a6c72",
+    "url": "/realworld/static/js/runtime-main.0f5365ee.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rr-blog/precache-manifest.5b4fdbf17956390867c106129551bf29.js"
+  "/realworld/precache-manifest.2824549cf2dbadc3696395a314d7b649.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rr-blog/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/realworld/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
