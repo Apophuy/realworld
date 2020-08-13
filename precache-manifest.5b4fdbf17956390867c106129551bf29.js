7aec5cbd0f1449c341ab381d181aadffd8834331
Apophuy <apophuy@hotmail.com>
2020-08-13 22:20:34 +0500

    Updates

---
+++ precache-manifest.5b4fdbf17956390867c106129551bf29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38cca90649b6b51b104e336c1b641280",
+    "url": "/rr-blog/index.html"
+  },
+  {
+    "revision": "d6e414be6d782b212b54",
+    "url": "/rr-blog/static/css/2.56808b57.chunk.css"
+  },
+  {
+    "revision": "00eb24da6e2e6f4145cb",
+    "url": "/rr-blog/static/css/main.d095d89e.chunk.css"
+  },
+  {
+    "revision": "d6e414be6d782b212b54",
+    "url": "/rr-blog/static/js/2.048a5d04.chunk.js"
+  },
+  {
+    "revision": "4f98be436b88b05bb27c5253e1cce92a",
+    "url": "/rr-blog/static/js/2.048a5d04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00eb24da6e2e6f4145cb",
+    "url": "/rr-blog/static/js/main.335f0d9e.chunk.js"
+  },
+  {
+    "revision": "5175c9a7e5fea3c66efc",
+    "url": "/rr-blog/static/js/runtime-main.66248d9d.js"
+  }
+]);
